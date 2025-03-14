bd8fab064f659daf6f963e62cc2e89874152e972
ChrisToxz <chris@toxz.nl>
2025-03-14 20:47:16 +0100

    add CreateSnap action

---
+++ app/Actions/Snap/CreateSnap.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Actions\Snap;
+
+use App\Actions\StoreUploadedFile;
+use App\Models\Snap;
+use Illuminate\Http\Request;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class CreateSnap
+{
+    use AsAction;
+
+    public function handle(Request $request): Snap
+    {
+        $file = StoreUploadedFile::run($request->file('image'));
+
+        return $request->user()->snaps()->create([
+            'ident' => GenerateSnapIdentifier::run(),
+            'name' => $request->file('image')->getClientOriginalName(),
+            'description' => '',
+            'path' => $file,
+        ]
+        );
+    }
+}
