f0105d3ffbf1f6e1afeb8d1b31a7e7d5716bf207
ChrisToxz <chris@toxz.nl>
2025-03-14 17:30:47 +0100

    fix ident index

--- database/migrations/2025_03_13_200455_create_images_table.php
+++ database/migrations/2025_03_13_200455_create_images_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->ulid('id')->primary();
-            $table->string('ident')->primary();
+            $table->string('ident')->index();
             $table->string('name');
             $table->text('description')->nullable();
             $table->string('path');
