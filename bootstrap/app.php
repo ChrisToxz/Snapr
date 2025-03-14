b7dcb2a835e430a762e52a7ef6bf04c408b0159f
ChrisToxz <chris@toxz.nl>
2025-03-14 18:50:33 +0100

    fix: handle MethodNotAllowed for API with JSON response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -20,6 +21,14 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: ForceJsonRequestHeader::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (MethodNotAllowedHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Method not allowed',
+                ], 405);
+            }
+        });
+
         $exceptions->render(function (NotFoundHttpException $e, Request $request) {
             if ($request->is('api/*')) {
                 return response()->json([
