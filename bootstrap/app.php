927e8a55f77a936700f72f9853119a4e52f2d408
ChrisToxz <chris@toxz.nl>
2025-03-14 18:09:07 +0100

    fix: handle 404 for API routes with JSON response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,8 @@ use App\Http\Middleware\ForceLoginWhenLocal;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +20,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: ForceJsonRequestHeader::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Endpoint not found',
+                ], 404);
+            }
+        });
     })->create();
