78939fba92446ef5d02a328155f453a79fe2fcbf
ChrisToxz <chris@toxz.nl>
2025-03-16 14:27:28 +0100

    Only show infinite loading if there snaps

--- resources/views/livewire/snap-list.blade.php
+++ resources/views/livewire/snap-list.blade.php
@@ -5,6 +5,17 @@
                 <x-snap-card :snap="$snap" />
             @endforeach
         </div>
+        <div x-data="{ theEnd: false }" x-on:end-of-snaps.window="theEnd = true">
+            <div x-show="!theEnd" x-intersect.full="$wire.loadMore()" class="flex justify-center p-4">
+                <div wire:loading wire:target="loadMore">
+                    <x-loading />
+                </div>
+            </div>
+
+            <template x-if="theEnd">
+                <p class="mt-4 text-center">No more snaps to load!</p>
+            </template>
+        </div>
     @else
         <div class="flex h-screen w-full items-center justify-center text-xl italic">
             <div class="mt-[-33.33vh] text-center">
@@ -14,17 +25,5 @@
             </div>
         </div>
     @endif
-    <div x-data="{ theEnd: false }" x-on:end-of-snaps.window="theEnd = true">
-        <div x-show="!theEnd" x-intersect.full="$wire.loadMore()" class="flex justify-center p-4">
-            <div wire:loading wire:target="loadMore">
-                <x-loading />
-            </div>
-        </div>
-
-        <template x-if="theEnd">
-            <p class="mt-4 text-center">No more snaps to load!</p>
-        </template>
-    </div>
-
     <livewire:edit-snap-modal />
 </div>
