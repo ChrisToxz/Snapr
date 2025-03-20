5f5098dd11ddc9b7c6039c3aeab5a878c3e179ba
ChrisToxz <chris@toxz.nl>
2025-03-20 19:15:24 +0100

    add meta

--- resources/views/snap/show.blade.php
+++ resources/views/snap/show.blade.php
@@ -1,4 +1,8 @@
 <x-dynamic-component :component="auth()->check() ? 'app-layout' : 'guest-layout'">
+    <meta property="og:title" content="{{ $snap->name }}" />
+    <meta property="og:description" content="A screenshot from CT" />
+    <meta property="og:image" content="{{ asset("storage/snaps/$snap->path") }}" />
+    <meta property="og:url" content="{{ asset("storage/snaps/$snap->path") }}" />
     <div class="flex h-screen items-center justify-center">
         <div class="card bg-base-200 w-full rounded-xl shadow-xl">
             <figure class="my-10">
