5dfe8d6cd82cc3ea8ae5a056f64bc87f843df1e5
ChrisToxz <chris@toxz.nl>
2025-03-21 19:25:06 +0100

    refactor: Only keep image meta property for bigger previews

--- resources/views/snap/show.blade.php
+++ resources/views/snap/show.blade.php
@@ -1,5 +1,7 @@
 <x-dynamic-component :component="auth()->check() ? 'app-layout' : 'guest-layout'">
     <meta property="og:image" content="{{ asset("storage/snaps/$snap->path") }}" />
+    {{-- <meta name="twitter:card" content="summary_large_image"> --}}
+    <meta name="twitter:image" content="{{ asset("storage/snaps/$snap->path") }}" />
     <div class="flex h-screen items-center justify-center">
         <div class="card bg-base-200 w-full rounded-xl shadow-xl">
             <figure class="my-10">
